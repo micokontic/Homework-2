5c921fb3b1b14a2667b942145e7c0a5ade159120
Mico Kontic <micokontic@gmail.com>
2020-11-09 01:11:14 +0100

    task 3 right string resolved

--- zadatak3/script.js
+++ zadatak3/script.js
@@ -2,9 +2,14 @@ var inputField = document.getElementById('input-field');
 var boxes = document.getElementById('boxes');
 var addBox = document.getElementById('add-box');
 var regex = RegExp('([A-Z])|([a-z])|( )');
+var charArray=[];
+var num=0;
+var letterArray=[];
+var letterIndicator=[];
 
 inputField.addEventListener('keyup',getInputValue);
 addBox.addEventListener('click',addNewElement);
+
 boxes.addEventListener('keyup',function(e){
     if(e.target && e.target.parentNode.matches('div.box')){
         letterInput(e);
@@ -13,12 +18,14 @@ boxes.addEventListener('keyup',function(e){
 
 function getInputValue(e){
     console.log('pozvao sam je')
-    var num=e.target.value;
+    num=e.target.value;
     if(e.which==13){
         boxes.style.display='flex';
         document.getElementById("input-container").style.display="none";
         for(i=0;i<num;i++){
             addNewEmptyElement();
+            letterIndicator[i]=0;
+            letterArray[i]='';
         }
     }
 }
@@ -35,18 +42,32 @@ function addNewEmptyElement(){
 
 function addNewElement(){
     addNewEmptyElement();
+    num++;
 }
 
 function letterInput(e){
     var letter=e.target.value;
+    var childIndex = whichChild(e.target.parentNode)-1;
+
     if(letterCheck(letter)){
         e.target.parentNode.classList.remove('error');
+        letterArray[childIndex]=(e.target.value).toLowerCase();
+        letterIndicator[childIndex]=1;
+            if(letterIndicator.reduce((a, b) => a + b, 0)==num){
+                console.log('sva slova su ok')
+            }
+        
     }else{
         e.target.parentNode.classList.add('error');
+        letterIndicator[childIndex]=0;
+        letterArray[childIndex]='';
+
     }
     if(e.target.value==''){
         e.target.parentNode.classList.remove('error');
     }
+    console.log(letterArray);
+    console.log(letterIndicator);
 }
 
 function letterCheck(letter){
@@ -55,4 +76,10 @@ function letterCheck(letter){
     }else{
         return false;
     }
+}
+
+function whichChild(elem){
+    var  i= 0;
+    while((elem=elem.previousSibling)!=null) ++i;
+    return i;
 }
